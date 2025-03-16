1ef0a45615e1c04b5b2956df0db114a91932347e
Charity Rymbai <charityrymbai@hotmail.com>
2025-03-17 03:52:43 +0530

    improved dashboard-ui

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import './globals.css';
+import { ThemeProvider } from "./components/ThemeProviders";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -22,7 +23,9 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ThemeProvider>
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
